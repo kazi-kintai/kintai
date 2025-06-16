3e8df0558b2f352cbb16fbfd38d303f1419b703c
zhaomanjia <gksi.kenshu.202513@gmail.com>
2025-06-16 11:29:12 +0900

    名前変更、まる消し

--- src/main/java/kintai/WorkPunchServlet.java
+++ src/main/java/kintai/WorkPunchServlet.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 
 @WebServlet("/WorkControl")
-public class WorkControlServlet extends HttpServlet {
+public class WorkPunchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
